dcfaefb52d3aefc9c12b85e9181e7472d2104deb
David Taccetta <taccetta14@gmail.com>
2022-06-03 12:34:50 -0400

    Turned Response into a Method

--- SyntaxSugarExercise/Program.cs
+++ SyntaxSugarExercise/Program.cs
@@ -6,6 +6,10 @@ namespace SyntaxSugarExercise
     {
         static void Main(string[] args)
         {
+            Response();
+        }
+        public static void Response()
+        {
             var answer = 4;
             var response = (answer < 9) ? $"{answer} is less than nine." : $"{answer} is greater than or equal to nine";
             Console.WriteLine(response);
